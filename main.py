1e9ead17a2bc428528d3e817c6b663311b299187
G M O <30497886+boyfriendnibluefairy@users.noreply.github.com>
2025-12-01 17:25:34 +0100

    performed major refactoring for easy debugging

---
+++ main.py
@@ -0,0 +1,6 @@
+"""
+    main.py is left intentionally blank so that you can easily cut-and-paste code from any of the scripts inside
+    ch01_examples/archived_examples/
+
+    The description of each script is presented in README.md
+"""
