9cca03be797542af2400c81fb3b14a5b51ce81ed
G M O <30497886+boyfriendnibluefairy@users.noreply.github.com>
2025-12-09 11:46:47 +0100

    matlab scripts and comments

---
+++ ch07_machine_learning/matlab_wrappers/matlab_sandbox_01_review/s01_scripts_and_comments.m
@@ -0,0 +1,12 @@
+
+% This is a comment
+
+% s01, in my context, means script 01
+
+clc     % clear command window
+
+% Use semi-colon to suppress output on the command window.
+% Example
+x = 'opticsml'
+y = 'cpu';
+z = 'pytorch'
